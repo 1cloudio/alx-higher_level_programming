ec7a2be687c74ed5e41f8029fd096e2ea5f216b7
Nnachi Michael <cloudio4luv2009@gmail.com>
2022-12-01 07:38:46 -0800

    python hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+import hidden_4
 
-    import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".formats(s))
+array = dir(hidden_4)
+if __name__ == "__main__":
+    for i in array:
+        if i[:2] != '__':
+            print(i)
