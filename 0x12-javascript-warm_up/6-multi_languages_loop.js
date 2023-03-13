985a0a26c21168f48ed62ba35d6c4eb75c280e8d
Nnachi Michael <cloudio4luv2009@gmail.com>
2023-03-13 13:58:32 +0100

    JS warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const texts = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
-  ];
-  
-  for (let i = 0; i < texts.length; i++) {
-    console.log(texts[i]);
-  }
+const arrString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arrString.length; i++) {
+  console.log(arrString[i]);
+}
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-/**
- * add - Computes the sum of 2 numbers.
- * @param {Number} a - The first number.
- * @param {Number} b - The second number.
- *
- * @returns The sum of the 2 numbers.
- */
 function add (a, b) {
-    return a + b;
+    console.log(a + b);
   }
   
-  console.log(
-    add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
-  );
+  let firstArg = process.argv[2];
+  let secondArg = process.argv[3];
+  
+  firstArg = parseInt(firstArg);
+  secondArg = parseInt(secondArg);
+  
+  add(firstArg, secondArg);
