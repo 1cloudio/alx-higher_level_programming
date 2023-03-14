7ff91aa5b3ce40709a387d32d0bc81285224629d
Nnachi Michael <cloudio4luv2009@gmail.com>
2023-03-14 12:20:08 +0100

    JS objects scopes closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -8,10 +8,10 @@ class Rectangle {
      * @param {Number} w The value of the width.
      * @param {Number} h The value of the height.
      */
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
-    }
+constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
-  
-  module.exports = Rectangle;
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -8,12 +8,12 @@ class Rectangle {
      * @param {Number} w The value of the width.
      * @param {Number} h The value of the height.
      */
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
+}
+
+module.exports = Rectangle;
