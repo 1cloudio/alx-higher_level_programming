87f4272aede3dbdcff7c12bc868da65e57eba02d
Nnachi Michael <cloudio4luv2009@gmail.com>
2023-03-18 10:53:21 +0100

    Python Object Relational Mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-'''Prints all rows in the states table of a database.
-'''
-import sys
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
+import sys
 
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 4:
-        db_connection = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
         )
-        cursor = db_connection.cursor()
-        cursor.execute('SELECT * FROM states ORDER BY id ASC;')
-        results = cursor.fetchall()
-        for result in results:
-            print(result)
-        db_connection.close()
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    table = cur.fetchall()
+
+    for row in table:
+        print(row)
+
+    cur.close()
+    db.close()
